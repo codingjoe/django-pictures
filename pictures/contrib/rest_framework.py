0ab6d173c06b8e336d1acde210060f4ca5a115a1
Johannes Maron <johannes@maron.family>
2023-02-08 13:24:23 +0100

    Fix #89 -- Handle invalid container values in DRF
    A user may pass any container value to the endpoint, and we need
    to ensure proper error handling and error messages.
    
    Co-authored-by: truongvan <truongvan@users.noreply.github.com>

--- pictures/contrib/rest_framework.py
+++ pictures/contrib/rest_framework.py
@@ -1,5 +1,6 @@
 import json
 
+from django.http import QueryDict
 from rest_framework import serializers
 
 __all__ = ["PictureField"]
@@ -34,12 +35,18 @@ class PictureField(serializers.ReadOnlyField):
             },
         }
         try:
-            query_params = self.context["request"].GET
+            query_params: QueryDict = self.context["request"].GET
         except KeyError:
             pass
         else:
             ratio = query_params.get(f"{self.source}_ratio")
             container = query_params.get(f"{self.source}_container")
+            try:
+                container = int(container)
+            except TypeError:
+                container = None
+            except ValueError as e:
+                raise ValueError(f"Container width is not a number: {container}") from e
             breakpoints = {
                 bp: int(query_params.get(f"{self.source}_{bp}"))
                 for bp in get_settings().BREAKPOINTS
